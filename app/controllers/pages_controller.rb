d175157e420de15b1a668ec46119fe2e51b438d0
Jacques Crocker <jcnetdev@gmail.com>
2008-07-28 04:43:59 -0700

    Update pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,4 @@ class PagesController < ApplicationController
   def about
   end
   
-  def blog
-    if AppConfig.blog_url
-      redirect_to AppConfig.blog_url
-    else
-      flash[:notice] = "Blog coming soon..."
-      redirect_to root_url
-    end
-  end
 end
